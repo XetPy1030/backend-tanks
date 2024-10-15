8e4c69209751df0f12218c94116ebbb57cd9bc01
XetPy1030 <xetpy1030@mail.ru>
2024-10-15 15:16:53 +0300

    Init project

---
+++ backend/tests/test_main.py
@@ -0,0 +1,23 @@
+import pytest
+from httpx import ASGITransport, AsyncClient
+from starlette.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {'Hello': 'World'}
+
+
+@pytest.mark.anyio
+async def test_root():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {'Hello': 'World'}
